4826f20137d6036889f2bbd0f3109b96d84523f5
codingInSpace <jonathan.grangien@gmail.com>
2015-12-18 13:52:10 +0100

    Add sprite

--- game/main.js
+++ game/main.js
@@ -1,10 +1,15 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+var sprite;
+
 function preload() {
+  game.load.spritesheet('guy', 'assets/sprites/pablo.png', 41, 58);
 }
 
 function create() {
+  sprite = game.add.sprite(0, 0, 'guy');
 }
 
 function update() {
+
 }
